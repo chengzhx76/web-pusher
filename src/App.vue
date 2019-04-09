e5556487724819c408501e261e7c3e7de8fd9f93
chengzhx76 <chengzhx76@qq.com>
2019-04-09 22:30:59 +0800

    基础框架

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
   <div id="app">
+    <header class="header">
+      <div class="sidebar">
+        <div class="logo">Pusher</div>
+        <div class="nav">登录</div>
+      </div>
+    </header>
     <router-view></router-view>
   </div>
 </template>
@@ -8,14 +14,33 @@
   export default {
     name: 'app',
     components:{
-    },
+    }
   }
 </script>
 
-<style>
-  * {
-    padding: 0;
-    margin: 0;
+<style rel="stylesheet/scss" lang="scss" scoped>
+  @import "@/style/mixin.scss";
+  @import "@/style/variables.scss";
+  .header {
+    @include height-px-width-100(80);
+    @include justify-align-center;
+    background: darkseagreen;
+    .sidebar {
+      @include height-scale-100-width-scale(80%);
+      @include justify-space-between-align-center;
+      background: darkkhaki;
+      .logo {
+        @include height-scale-100-width-px(120);
+        @include text-center(80);
+        font-size: 26px;
+        color: #3D94FE;
+        background: fuchsia;
+      }
+      .nav {
+        @include height-scale-100-width-px(80);
+        @include text-center(80);
+      }
+    }
   }
 
 </style>
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,13 +1,8 @@
 <template>
   <div id="login" class="login-container">
-    <header class="header">
-      <div class="sidebar">
-        <div class="logo"></div>
-        <div class="nav">ddd</div>
-      </div>
-    </header>
-
-
+    <div class="main">
+      dff
+    </div>
   </div>
 </template>
 
@@ -36,14 +31,13 @@
 <style rel="stylesheet/scss" lang="scss" scoped>
   @import "@/style/mixin.scss";
   @import "@/style/variables.scss";
-  .header {
-    @include height-px-width-100(80);
-    @include justify-align-center;
-    background: darkseagreen;
-    .sidebar {
-      @include height-scale-100-width-scale(80%);
-      background: darkkhaki;
-    }
+  .login-container {
+    height: calc(100% - 80px);
+    width: 100%;
+  }
+  .main {
+    @include height-width-scale-100;
+    background: #fa4d5c;
   }
 
 </style>
