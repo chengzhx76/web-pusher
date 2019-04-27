71c4db256c93469db294cd83b65c90fb027d2da5
chengzhx76 <chengzhx76@qq.com>
2019-04-27 15:32:36 +0800

    基础框架

--- src/views/channel/setting.vue
+++ src/views/channel/setting.vue
@@ -1,6 +1,25 @@
 <template>
   <div id="setting">
-    <div class="title">还没有可用的通道呢</div>
+    <div class="base">
+      <div class="title">基本信息</div>
+      <form class="info">
+        <div class="input input-name">
+          <label for="name">通道名字</label>
+          <input type="text" id="name">
+        </div>
+        <div class="input input-email">
+          <label for="email">联系邮箱</label>
+          <input type="text" id="email">
+        </div>
+      </form>
+      <div class="button-group">
+
+      </div>
+    </div>
+    <div class="qrcode">
+      <div class="title">订阅二维码</div>
+    </div>
+
   </div>
 </template>
 
@@ -30,8 +49,41 @@
   @import "@/style/mixin.scss";
   @import "@/style/variables.scss";
   #setting {
-    height: calc(100% - 60px);
     width: 100%;
+  }
+  .title {
+    @include height-px-width-100(36);
+    line-height: 36px;
+    font-size: 18px;
+    color: #E5649C;
+    text-indent: 8px;
+    font-weight: bold;
+    background: #F7F5F6;
+  }
+  .base {
+    .info {
+      width: 100%;
+      .input {
+        @include height-px-width-100(55);
+        margin-top: 3px;
+        label {
+          @include height-px-width-100(29);
+          display: block;
+          line-height: 40px;
+          text-indent: 5px;
+        }
+        input {
+          @include height-px-width-100(25);
+          display: block;
+          text-indent: 5px;
+          line-height: 35px;
+          border-top: none;
+          border-left: none;
+          border-right: none;
+          border-bottom: 1px solid #cccccc;
+        }
+      }
+    }
   }
 
 
